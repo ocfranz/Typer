df7fcde506f4e121e142dc9f0393d4eac6a60c31
Franz Ogosi Cahuana <franz.ogosi@usil.pe>
2020-08-24 23:47:14 -0500

    adding react-helmet and fonts changed

--- src/app/styles/globalStyles.js
+++ src/app/styles/globalStyles.js
@@ -1,40 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-import GilroyBoldWoff from '../assets/fonts/Gilroy-Bold.woff';
-import GilroyBoldWoff2 from '../assets/fonts/Gilroy-Bold.woff2';
-import GilroyLightWoff from '../assets/fonts/Gilroy-Light.woff';
-import GilroyLightWoff2 from '../assets/fonts/Gilroy-Light.woff2'; 
-import GilroyRegularWoff from '../assets/fonts/Gilroy-Regular.woff'; 
-import GilroyRegularWoff2 from '../assets/fonts/Gilroy-Regular.woff2'; 
-
 const GlobalStyles = createGlobalStyle`
-    @font-face {
-        font-family: 'Gilroy';
-        src: url(${GilroyRegularWoff2}) format('woff2'),
-            url(${GilroyRegularWoff}) format('woff');
-        font-weight: normal;
-        font-style: normal;
-        font-display: swap;
-    }
-
-    @font-face {
-        font-family: 'Gilroy';
-        src: url(${GilroyLightWoff2}) format('woff2'),
-            url(${GilroyLightWoff}) format('woff');
-        font-weight: 300;
-        font-style: normal;
-        font-display: swap;
-    }
-
-    @font-face {
-        font-family: 'Gilroy';
-        src: url(${GilroyBoldWoff2}) format('woff2'),
-            url(${GilroyBoldWoff}) format('woff');
-        font-weight: bold;
-        font-style: normal;
-        font-display: swap;
-    }
-
     body, html{
         border : 0;
         margin :0;
@@ -52,7 +18,7 @@ const GlobalStyles = createGlobalStyle`
     }
     
     *{
-        font-family: 'Gilroy';
+        font-family: 'DM Sans', sans-serif;
     }
     [contenteditable][placeholder]:empty:before {
         content: attr(placeholder);
